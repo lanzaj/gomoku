632514adab2c358bb068f1ad8ea66ed55e36e872
Gregoire Gobert <gregoire.gobert@gmail.com>
2025-09-22 11:52:28 +0200

    save.22-09

--- backend/gomoku.d
+++ backend/gomoku.d
@@ -1,9 +1,15 @@
 gomoku: src/Main.cpp include/Gomoku.hpp include/Board.hpp \
   include/Player.hpp include/Cell.hpp include/Server.hpp \
   libs/nlohmann/json.hpp
+
 include/Gomoku.hpp:
+
 include/Board.hpp:
+
 include/Player.hpp:
+
 include/Cell.hpp:
+
 include/Server.hpp:
+
 libs/nlohmann/json.hpp:
